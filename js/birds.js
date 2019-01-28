f7234694d80a4c9600f13c9391db277cbdbb13b4
Scott Gruber <scott@scottgruber.me>
2019-01-27 18:06:16 -0800

    flock of blue jays

---
+++ js/birds.js
@@ -0,0 +1,219 @@
+
+var bluejay = document.getElementById('bluejay')
+TweenMax.fromTo(
+  '.bluejay:nth-child(1)',
+  3,
+  {
+    backgroundColor: '#ffc4b8',
+    transform: 'translateX(100vw)'
+  },
+  {
+    backgroundColor: '#dedebd',
+    transform: 'translateX(0)'
+  }
+)
+
+TweenMax.staggerFromTo(
+  '.bluejay:nth-child(2)',
+  2,
+  {
+    backgroundColor: '#fe5b5e',
+    transform: 'translateY(100vh)'
+  },
+  {
+    backgroundColor: '#BFB3A8',
+    transform: 'translateY(0)'
+  },
+  0.7
+)
+
+TweenMax.staggerFromTo(
+  '.bluejay:nth-child(3)',
+  1,
+  {
+    backgroundColor: '#e54a46',
+    transform: 'translateY(100vh)'
+
+  },
+  {
+    backgroundColor: '#bd8a96',
+    transform: 'translateY(0)'
+  },
+  1.4
+)
+
+TweenMax.staggerFromTo(
+  '.bluejay:nth-child(4)',
+  2,
+  {
+    backgroundColor: '#a2838c',
+    transform: 'translateY(100vh)'
+  },
+  {
+    backgroundColor: '#BAC96D',
+    transform: 'translateY(0)'
+  },
+  0.9
+)
+
+TweenMax.staggerFromTo(
+  '.bluejay:nth-child(5)',
+  3,
+  {
+    backgroundColor: '#ff817f',
+    transform: 'translateY(100vh)'
+  },
+  {
+    backgroundColor: '#F6D763',
+    transform: 'translateY(0)'
+  },
+  0.9
+)
+
+TweenMax.staggerFromTo(
+  '.bluejay:nth-child(6)',
+  1,
+  {
+    backgroundColor: '#e54a46',
+    transform: 'translateY(100vh)'
+
+  },
+  {
+    backgroundColor: '#adcfbf',
+    transform: 'translateY(0)'
+  },
+  1.4
+)
+
+TweenMax.staggerFromTo(
+  '.bluejay:nth-child(7)',
+  3,
+  {
+    backgroundColor: '#70504e',
+    transform: 'translateY(100vh)'
+  },
+  {
+    backgroundColor: '#4B89BF',
+    transform: 'translateY(0)'
+  },
+  0.9
+)
+
+TweenMax.staggerFromTo(
+  '.bluejay:nth-child(8)',
+  3,
+  {
+    backgroundColor: '#454543',
+    transform: 'translateY(100vh)'
+  },
+  {
+    backgroundColor: '#F0E7D8',
+    transform: 'translateY(0)'
+  },
+  0.4
+)
+
+TweenMax.staggerFromTo(
+  '.bluejay:nth-child(9)',
+  3,
+  {
+    backgroundColor: '#def7fa',
+    transform: 'translateY(100vh)'
+  },
+  {
+    backgroundColor: '#def7fa',
+    transform: 'translateY(0)'
+  },
+  0.9
+)
+
+TweenMax.fromTo(
+  '.bluejay:nth-child(10)',
+  3,
+  {
+    backgroundColor: '#4F643D',
+    transform: 'translateY(100vh)'
+  },
+  {
+    backgroundColor: '#03ad87',
+    transform: 'translateY(0)'
+  }
+)
+
+TweenMax.fromTo(
+  '.bluejay:nth-child(11)',
+  3,
+  {
+    backgroundColor: '#4F643D',
+    transform: 'translateY(100vh)'
+  },
+  {
+    backgroundColor: '#ffb382',
+    transform: 'translateY(0)'
+  }
+)
+
+TweenMax.fromTo(
+  '.bluejay:nth-child(12)',
+  3,
+  {
+    backgroundColor: '#4F643D',
+    transform: 'translateX(100vh)'
+  },
+  {
+    backgroundColor: '#616694',
+    transform: 'translateX(0)'
+  }
+)
+
+TweenMax.fromTo(
+  '.bluejay:nth-child(13)',
+  3,
+  {
+    backgroundColor: '#4F643D',
+    transform: 'translateY(100vh)'
+  },
+  {
+    backgroundColor: '#F6D763',
+    transform: 'translateY(0)'
+  }
+)
+
+TweenMax.fromTo(
+  '.bluejay:nth-child(14)',
+  3,
+  {
+    backgroundColor: '#4F643D',
+    transform: 'translateY(100vh)'
+  },
+  {
+    backgroundColor: '#8fb054',
+    transform: 'translateY(0)'
+  }
+)
+
+TweenMax.fromTo(
+  '.bluejay:nth-child(15)',
+  3,
+  {
+    backgroundColor: '#4F643D',
+    transform: 'translateY(100vh)'
+  },
+  {
+    backgroundColor: '#f5db99',
+    transform: 'translateY(0)'
+  }
+)
+
+TweenMax.fromTo(
+  '.bluejay:nth-child(16)',
+  3,
+  {
+    backgroundColor: '#4F643D',
+    transform: 'translateX(100vh)'
+  },
+  {
+    backgroundColor: '#b5bdf0',
+    transform: 'translateX(0)'
+  }
+)
